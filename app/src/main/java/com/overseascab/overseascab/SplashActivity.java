c00eeb58018e450d193c91cbc94d569f38d91f0c
RaviSharma <ravik6373@gmail.com>
2018-09-19 09:04:03 +0530

    Initial commit

---
+++ app/src/main/java/com/overseascab/overseascab/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.overseascab.overseascab;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ProgressBar pb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        pb = (ProgressBar)findViewById(R.id.progress);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+
+    }
+}
